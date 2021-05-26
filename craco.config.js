d39ceeddc281995bcd14c11958b83ba2dbe23b34
Carlos Silva <csilva2810@gmail.com>
2021-05-26 19:52:09 -0300

    Add craco config and bundle analyzer

---
+++ craco.config.js
@@ -0,0 +1,19 @@
+const { whenProd } = require('@craco/craco');
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = () => {
+  const analyzerMode = process.env.REACT_APP_INTERACTIVE_ANALYZE
+    ? 'server'
+    : 'json';
+
+  return {
+    webpack: {
+      plugins: {
+        add: [
+          ...whenProd(() => [new BundleAnalyzerPlugin({ analyzerMode })], []),
+        ],
+      },
+    },
+  };
+};
